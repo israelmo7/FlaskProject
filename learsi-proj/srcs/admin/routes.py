70273a0556552df035cbc000ed160c0022c7082c
learsi <learsi@MacBook-Pro-sl-Israel.local>
2026-09-22 17:38:00 +0300

    changes

--- learsi-proj/srcs/admin/routes.py
+++ learsi-proj/srcs/admin/routes.py
@@ -4,17 +4,25 @@ admin_bp = Blueprint(
     'admin_bp', __name__, template_folder='templates', static_folder='static'
 )
 
+rooms_c, keys_c, guests_c = 0, 0, 0
 
-@admin_bp.route('/<kid>', methods=['GET'])
-def admin_login(kid):
+
+def init_db_a(r, k, g):
+    global rooms_c, keys_c, guests_c
+    rooms_c, keys_c, guests_c = r, k, g
+
+
+@admin_bp.route('/', methods=['GET'])
+def admin_login():
     guest_id = session.get('id', '')[:8] if session.get('id') else None
     if not guest_id:
         return '', 403
 
-    pocket = guests_c.get_guest(guest_id)
+    pocket = guests_c.get_pocket(guest_id)
     if not pocket:
         return '', 403
 
+    pocket = pocket[0]
     if pocket != kid:
         return '', 403
 
